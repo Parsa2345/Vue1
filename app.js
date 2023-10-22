249d27072bfb10fcc707c93f7eaf0b9be49c74b5
Parsa2345 <jack.kamard@gmail.com>
2023-10-22 08:00:00 +0330

    v-for index

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const app = Vue.createApp({
- 
+
     data() {
         return {
-            name:"ali",
-            family :"Parsa",
-            isShow: false
+            name: "ali",
+            family: "Parsa",
+            isShow: false,
+            users: ["ali", "naghi", "gholi", "sall", "joe"],
+            contacts: [
+                { name: "Mohsen", family: "Parsa" },
+                { name: "ali", family: "alavi" },
+                { name: "naghi", family: "naghavi" },
+                { name: "hasan", family: "hasani" },
+                { name: "sara", family: "boor" },
+                { name: "joe", family: "bing" },
+
+            ]
         }
     },
     methods: {
-        toggleStatus(){
-            this.isShow=!this.isShow;
+        toggleStatus() {
+            this.isShow = !this.isShow;
         }
 
     },
