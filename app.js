48096715b3e804f644f00aefd86bf7ea1eb1a07a
Parsa2345 <jack.kamard@gmail.com>
2023-10-22 06:51:38 +0330

    first

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
- app = Vue.createApp({
-    template :"<h1> Mohsen Parsanpm </h1>"
+const app = Vue.createApp({
+    // template:"<h1>  {{name}} {{family}}</h1>",
+    data() {
+        return {
+            name:"Mohsen",
+            family :"Parsa",
+            counter: 0
+        }
+    },
+    methods: {
+        Increase()
+        {
+            this.counter++;
+        },
 
- });
+        Decrease(counter){
+            if(counter>0)
+            {this.counter--;}
 
- app.mounted("#app");
+        },
+        GetPos(event){
+            console.log(`x is ${event.x} and y is ${event.y}`);
+        }
+
+    },
+});
+app.mount("#app");
